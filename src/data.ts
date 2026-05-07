ed4b646a1a41b013456fb5273e80d22a1f2ecea7
faye <goncalves.livi30@gmail.com>
2026-05-07 14:00:40 -0300

    informações adicionais livros abril e maio

--- src/data.ts
+++ src/data.ts
@@ -41,7 +41,9 @@ export const booksByMonth: Record<string, { id: number; title: string; author: s
             author: 'Neil Gaiman',
             cover: '/coraline.capa.jpg',
             synopsis: 'Coraline conta a história de uma menina que descobre uma porta secreta em sua nova casa. Ao atravessá-la, encontra uma versão alternativa de sua vida — aparentemente perfeita, mas que esconde segredos sombrios. Uma narrativa envolvente que mistura fantasia e suspense, explorando coragem, identidade e escolhas.',
-            category: 'Fantasia / Terror leve / Juvenil'
+            category: 'Fantasia / Terror leve / Juvenil',
+            releaseDate: '2002',
+            pages: 224
         }
     ],
     "2026-05": [
@@ -50,7 +52,9 @@ export const booksByMonth: Record<string, { id: number; title: string; author: s
             title: 'A Cabeça do Santo',
             author: 'Socorro Acioli',
             cover: '/a cabeça do santo.jpg',
-            synopsis: 'Pouco antes de morrer, a mãe de Samuel lhe faz um último pedido: que ele vá encontrar a avó e o pai que nunca conheceu. Mesmo contrariado, o rapaz cumpre a promessa e faz a pé o caminho de Juazeiro do Norte até a pequena cidade de Candeia, sofrendo todas as agruras do sol impiedoso do sertão do Ceará.\n\nAo chegar àquela cidade quase fantasma, ele encontra abrigo num lugar curioso: a cabeça oca e gigantesca de uma estátua inacabada de santo Antônio, que jazia separada do resto do corpo. Mas as estranhezas não param aí: Samuel começa a escutar uma confusão de vozes femininas apenas quando está dentro da cabeça. Assustado, se dá conta de que aquilo são as preces que as mulheres fazem ao santo falando de amor.\n\nSeu primeiro contato na cidade será com Francisco, um rapaz de quem logo fica amigo e que resolve ajudá-lo a explorar comercialmente o seu dom da escuta, promovendo casamentos e outras artimanhas amorosas. Antes parada no tempo, a cidade aos poucos volta à vida, à medida que vai sendo tomada por fiéis de todos os cantos, atraídos pelo poder inaudito de Samuel. Em meio a esse tumulto, ele ainda irá se apaixonar por uma voz misteriosa que se destaca entre as tantas outras que ecoam na cabeça do santo.'
+            synopsis: 'Pouco antes de morrer, a mãe de Samuel lhe faz um último pedido: que ele vá encontrar a avó e o pai que nunca conheceu. Mesmo contrariado, o rapaz cumpre a promessa e faz a pé o caminho de Juazeiro do Norte até a pequena cidade de Candeia, sofrendo todas as agruras do sol impiedoso do sertão do Ceará.\n\nAo chegar àquela cidade quase fantasma, ele encontra abrigo num lugar curioso: a cabeça oca e gigantesca de uma estátua inacabada de santo Antônio, que jazia separada do resto do corpo. Mas as estranhezas não param aí: Samuel começa a escutar uma confusão de vozes femininas apenas quando está dentro da cabeça. Assustado, se dá conta de que aquilo são as preces que as mulheres fazem ao santo falando de amor.\n\nSeu primeiro contato na cidade será com Francisco, um rapaz de quem logo fica amigo e que resolve ajudá-lo a explorar comercialmente o seu dom da escuta, promovendo casamentos e outras artimanhas amorosas. Antes parada no tempo, a cidade aos poucos volta à vida, à medida que vai sendo tomada por fiéis de todos os cantos, atraídos pelo poder inaudito de Samuel. Em meio a esse tumulto, ele ainda irá se apaixonar por uma voz misteriosa que se destaca entre as tantas outras que ecoam na cabeça do santo.',
+            releaseDate: '2014',
+            pages: 174
         }
     ]
 };
